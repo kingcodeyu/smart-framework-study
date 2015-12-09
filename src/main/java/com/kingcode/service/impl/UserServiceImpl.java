7a584b18fbf02dffe39c90d6439fb976e11a5175
jamesandroshiro@hotmail.com <jamesandroshiro@hotmail.com>
2015-12-09 11:16:05 +0800

    update

--- src/main/java/com/kingcode/service/impl/UserServiceImpl.java
+++ src/main/java/com/kingcode/service/impl/UserServiceImpl.java
@@ -11,5 +11,14 @@ public class UserServiceImpl implements UserService {
 	public void addUser(User user) {
 		System.out.println(ToStringBuilder.reflectionToString(user));
 	}
+	public void deleteUser(String uid){
+		System.out.println("id:::"+uid);
+	}
+	public void modifyUser(User user){
+		System.out.println("ok:::"+user);
+	}
+	public void noteUser(User user){
+		System.out.println("test::"+user);
+	}
 	
 }
